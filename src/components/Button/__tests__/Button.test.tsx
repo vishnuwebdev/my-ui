e826934630fa4db6c54bd11accce0ce02d51dba7
Vishnu Sharma <vishisone@yahoo.com.au>
2025-09-13 09:37:10 +0530

    Add yarn.lock file to build the app using prechecks

--- src/components/Button/__tests__/Button.test.tsx
+++ src/components/Button/__tests__/Button.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { fireEvent } from "@testing-library/dom";
 import { Button } from "../Button";
 import "@testing-library/jest-dom";
 
